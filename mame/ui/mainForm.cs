66ff8409475ca3fe05d6abfffbf7d97eabfea470
Simon Gillespie <>
2020-05-17 10:11:11 +0100

    Remove DirectX sournd

--- mame/ui/mainForm.cs
+++ mame/ui/mainForm.cs
@@ -11,8 +11,8 @@ using System.IO;
 using System.Threading;
 using System.Xml.Linq;
 using System.Runtime.InteropServices;
-using Microsoft.DirectX.DirectSound;
-using DSDevice = Microsoft.DirectX.DirectSound.Device;
+//using Microsoft.DirectX.DirectSound;
+//using DSDevice = Microsoft.DirectX.DirectSound.Device;
 using mame;
 //using cpu.nec;
 
@@ -35,8 +35,8 @@ namespace ui
         //public m72Form m72form;
         //public m92Form m92form;
         public string sSelect;
-        private DSDevice dev;
-        private BufferDescription desc1;
+        //private DSDevice dev;
+       // private BufferDescription desc1;
         public static Thread t1;
         public string handle1;
         [DllImport("user32.dll", EntryPoint = "ShowCursor", CharSet = CharSet.Auto)]
@@ -62,29 +62,22 @@ namespace ui
 
         public void ApplyRom()
         {
-            //if (listView1.SelectedItems.Count > 0)
-            //{
+
                 this.Close();
                 Mame.exit_pending = true;
                 Thread.Sleep(100);
-                //RomInfo.Rom = RomInfo.GetRomByName(listView1.SelectedItems[0].SubItems[2].Text);
-            //  RomInfo.Rom = RomInfo.GetRomByName("ffightu");
+
                 this.LoadRom();
                 if (Machine.bRom)
                 {
-                    //m68000Form.iStatus = 0;
-               //     m68000Form.iRAddress = 0;
-              //      m68000Form.iROp = 0;
-              //      m68000Form.iValue = 0;
-                    //z80Form.iStatus = 0;
+
                     Mame.exit_pending = false;
                     this.resetToolStripMenuItem.Enabled = true;
-                    //this..gameStripMenuItem.Enabled = true;
-                    //UI.ui_init(this);
+
                     mainForm.t1 = new Thread(Mame.mame_execute);
                     mainForm.t1.Start();
                 }
-           // }
+
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -97,17 +90,6 @@ namespace ui
             gameStripMenuItem.Enabled = false;
             Mame.sHandle1 = this.Handle.ToString();
             RomInfo.Rom=new RomInfo();
-            dev = new DSDevice();
-            dev.SetCooperativeLevel(this, CooperativeLevel.Normal);
-            desc1 = new BufferDescription();
-            desc1.Format = CreateWaveFormat();
-            desc1.BufferBytes = 0x9400;
-            desc1.ControlVolume = true;
-            desc1.GlobalFocus = true;
-            //Keyboard.InitializeInput(this);
-            //Sound.buf2 = new SecondaryBuffer(desc1, dev);
-
-           //InitLoadForm();
             RomInfo.Rom.Name="ffightu";
             RomInfo.Rom.Board = "CPS-1";
             RomInfo.Rom.Parent="ffight";
@@ -115,18 +97,6 @@ namespace ui
             RomInfo.Rom.Description="";
             RomInfo.Rom.Manufacturer="CapCom";
 
-            //InitCheatForm();
-            //InitCheatsearchForm();
-            //InitIpsForm();
-            InitM68000Form();
-            InitZ80Form();
-            //InitM6809Form();
-            //InitCpsForm();
-            //InitNeogeoForm();
-            //InitNamcos1Form();
-            //InitPgmForm();
-            //InitM72Form();
-            //InitM92Form();
             ApplyRom();
         }
         public void LoadRom()
@@ -142,7 +112,6 @@ namespace ui
             Machine.sDescription = Machine.rom.Description;
             Machine.sManufacturer = Machine.rom.Manufacturer;
             Machine.bRom = true;
-            //Machine.lsParents = RomInfo.GetParents(Machine.sName);
             Machine.lsParents = new List<String>();
             Machine.lsParents.Add("ffight");
             int i;
@@ -175,113 +144,7 @@ namespace ui
                     CPS.CPSInit();
                     //CPS.GDIInit();
                     break;
-                /*case "Neo Geo":
-                    Video.nMode = 1;
-                    itemSize = new ToolStripMenuItem[Video.nMode];
-                    for (i = 0; i < Video.nMode; i++)
-                    {
-                        itemSize[i] = new ToolStripMenuItem();
-                        itemSize[i].Size = new Size(152, 22);
-                        itemSize[i].Click += new EventHandler(itemsizeToolStripMenuItem_Click);
-                    }
-                    itemSize[0].Text = "320x224";
-                    resetToolStripMenuItem.DropDownItems.Clear();
-                    resetToolStripMenuItem.DropDownItems.AddRange(itemSize);                    
-                    Video.iMode = 0;
-                    itemSelect();
-                    neogeoToolStripMenuItem.Enabled = true;
-                    Neogeo.NeogeoInit();
-                    Neogeo.GDIInit();
-                    break;
-                case "Namco System 1":
-                    Video.nMode = 1;
-                    itemSize = new ToolStripMenuItem[Video.nMode];
-                    for (i = 0; i < Video.nMode; i++)
-                    {
-                        itemSize[i] = new ToolStripMenuItem();
-                        itemSize[i].Size = new Size(152, 22);
-                        itemSize[i].Click += new EventHandler(itemsizeToolStripMenuItem_Click);
-                    }
-                    itemSize[0].Text = "288x224";
-                    resetToolStripMenuItem.DropDownItems.Clear();
-                    resetToolStripMenuItem.DropDownItems.AddRange(itemSize);
-                    Video.iMode = 0;
-                    itemSelect();
-                    namcos1ToolStripMenuItem.Enabled = true;
-                    Namcos1.Namcos1Init();
-                    Namcos1.GDIInit();
-                    break;
-                case "IGS011":
-                    Video.nMode = 1;
-                    itemSize = new ToolStripMenuItem[Video.nMode];
-                    for (i = 0; i < Video.nMode; i++)
-                    {
-                        itemSize[i] = new ToolStripMenuItem();
-                        itemSize[i].Size = new Size(152, 22);
-                        itemSize[i].Click += new EventHandler(itemsizeToolStripMenuItem_Click);
-                    }
-                    itemSize[0].Text = "512x240";
-                    resetToolStripMenuItem.DropDownItems.Clear();
-                    resetToolStripMenuItem.DropDownItems.AddRange(itemSize);
-                    Video.iMode = 0;
-                    itemSelect();
-                    IGS011.GDIInit();
-                    IGS011.IGS011Init();
-                    break;
-                case "PGM":
-                    Video.nMode = 1;
-                    itemSize = new ToolStripMenuItem[Video.nMode];
-                    for (i = 0; i < Video.nMode; i++)
-                    {
-                        itemSize[i] = new ToolStripMenuItem();
-                        itemSize[i].Size = new Size(152, 22);
-                        itemSize[i].Click += new EventHandler(itemsizeToolStripMenuItem_Click);
-                    }
-                    itemSize[0].Text = "448x224";
-                    resetToolStripMenuItem.DropDownItems.Clear();
-                    resetToolStripMenuItem.DropDownItems.AddRange(itemSize);
-                    Video.iMode = 0;
-                    itemSelect();
-                    pgmToolStripMenuItem.Enabled = true;
-                    PGM.PGMInit();
-                    PGM.GDIInit();
-                    break;
-                case "M72":
-                    Video.nMode = 1;
-                    itemSize = new ToolStripMenuItem[Video.nMode];
-                    for (i = 0; i < Video.nMode; i++)
-                    {
-                        itemSize[i] = new ToolStripMenuItem();
-                        itemSize[i].Size = new Size(152, 22);
-                        itemSize[i].Click += new EventHandler(itemsizeToolStripMenuItem_Click);
-                    }
-                    itemSize[0].Text = "384x256";
-                    resetToolStripMenuItem.DropDownItems.Clear();
-                    resetToolStripMenuItem.DropDownItems.AddRange(itemSize);
-                    Video.iMode = 0;
-                    itemSelect();
-                    m72ToolStripMenuItem.Enabled = true;
-                    M72.M72Init();
-                    M72.GDIInit();
-                    break;
-                case "M92":
-                    Video.nMode = 1;
-                    itemSize = new ToolStripMenuItem[Video.nMode];
-                    for (i = 0; i < Video.nMode; i++)
-                    {
-                        itemSize[i] = new ToolStripMenuItem();
-                        itemSize[i].Size = new Size(152, 22);
-                        itemSize[i].Click += new EventHandler(itemsizeToolStripMenuItem_Click);
-                    }
-                    itemSize[0].Text = "320x240";
-                    resetToolStripMenuItem.DropDownItems.Clear();
-                    resetToolStripMenuItem.DropDownItems.AddRange(itemSize);
-                    Video.iMode = 0;
-                    itemSelect();
-                    m92ToolStripMenuItem.Enabled = true;
-                    M92.M92Init();
-                    M92.GDIInit();
-                    break;*/
+ 
             }
             if (Machine.bRom)
             {                
@@ -299,180 +162,11 @@ namespace ui
             switch (Machine.sName)
             {
                 case "drgnwrld":
-                    /*if (UI.single_step || Mame.paused)
-                    {
-                        Screen[] screen = Screen.AllScreens;
-                        this.Cursor = new Cursor(Cursor.Current.Handle);
-                        Cursor.Clip = screen[0].Bounds;
-                        ShowCursor();
-                    }
-                    else
-                    {
-                        Cursor.Clip = new Rectangle(new Point(this.Location.X + 7 + this.pictureBox1.Location.X, this.Location.Y + 29 + this.pictureBox1.Location.Y), this.pictureBox1.Size);
-                        HideCursor();
-                    }*/
+
                     break;
             }
         }
-        /*private void InitCheatForm()
-        {
-            cheatform = new cheatForm(this);
-            foreach (string sFile in Directory.GetFiles("cht"))
-            {
-                if (Path.GetExtension(sFile).ToLower() == ".cht")
-                {
-                    cheatform.cbCht.Items.Add(Path.GetFileNameWithoutExtension(sFile));
-                }
-            }
-            if (cheatform.cbCht.Items.Count > 0)
-            {
-                cheatform.cbCht.SelectedIndex = 0;
-            }
-        }*/
-        private void InitIpsForm()
-        {
-            /*ipsform = new ipsForm(this);
-            foreach (string sFile in Directory.GetFiles("ips"))
-            {
-                if (Path.GetExtension(sFile).ToLower() == ".cht")
-                {
-                    ipsform.cbCht.Items.Add(Path.GetFileNameWithoutExtension(sFile));
-                }
-            }
-            if (ipsform.cbCht.Items.Count > 0)
-            {
-                ipsform.cbCht.SelectedIndex = 0;
-            }*/
-        }
-        private void InitCheatsearchForm()
-        {
-            //cheatsearchform = new cheatsearchForm(this);
-        }
-        private void InitM68000Form()
-        {
-           // m68000form = new m68000Form(this);
-        }
-        private void InitZ80Form()
-        {
-           // z80form = new z80Form(this);
-        }
-        private void InitM6809Form()
-        {
-            //m6809form = new m6809Form(this);
-        }
-        private void InitCpsForm()
-        {
-            //cpsform = new cpsForm(this);
-        }
-        private void InitNeogeoForm()
-        {
-            //neogeoform = new neogeoForm(this);
-        }
-        private void InitNamcos1Form()
-        {
-            //namcos1form = new namcos1Form(this);
-        }
-        private void InitPgmForm()
-        {
-            //pgmform = new pgmForm(this);
-        }
-        private void InitM72Form()
-        {
-            //m72form = new m72Form(this);
-        }
-        /*private void InitM92Form()
-        {
-            m92form = new m92Form(this);
-        }*/
-        private void InitLoadForm()
-        {
-            //loadform = new loadForm(this);
-           ColumnHeader columnheader;
-            columnheader = new ColumnHeader();
-            columnheader.Text = "Title";
-            columnheader.Width = 350;
-           // loadform.listView1.Columns.Add(columnheader);
-            columnheader = new ColumnHeader();
-            columnheader.Text = "Year";
-            columnheader.Width = 60;
-          //  loadform.listView1.Columns.Add(columnheader);
-            columnheader = new ColumnHeader();
-            columnheader.Text = "ROM";
-            columnheader.Width = 90;
-          //  loadform.listView1.Columns.Add(columnheader);
-            columnheader = new ColumnHeader();
-            columnheader.Text = "Parent";
-            columnheader.Width = 60;
-//loadform.listView1.Columns.Add(columnheader);
-            columnheader = new ColumnHeader();
-            columnheader.Text = "Direction";
-            columnheader.Width = 70;
-    //        loadform.listView1.Columns.Add(columnheader);
-            columnheader = new ColumnHeader();
-            columnheader.Text = "Manufacturer";
-            columnheader.Width = 120;
-       //     loadform.listView1.Columns.Add(columnheader);
-            columnheader = new ColumnHeader();
-            columnheader.Text = "Board";
-            columnheader.Width = 120;
-         //   loadform.listView1.Columns.Add(columnheader);
-            XElement xe = XElement.Parse(mame.Properties.Resources.mame);
-            IEnumerable<XElement> elements = from ele in xe.Elements("game") select ele;
-            showInfoByElements(elements);
-            //loadRomList(elements);
-        }
-        private void showInfoByElements(IEnumerable<XElement> elements)
-        {
-            RomInfo.romList = new List<RomInfo>();
-            //StreamWriter sw1 = new StreamWriter("1.txt", false);
-            foreach (var ele in elements)
-            {
-                RomInfo rom = new RomInfo();
-                rom.Name = ele.Attribute("name").Value;
-                rom.Board = ele.Attribute("board").Value;
-                rom.Parent = ele.Element("parent").Value;
-                rom.Direction = ele.Element("direction").Value;
-                rom.Description = ele.Element("description").Value;
-                rom.Year = ele.Element("year").Value;
-                rom.Manufacturer = ele.Element("manufacturer").Value;
-                RomInfo.romList.Add(rom);
-                //loadform.listView1.Items.Add(new ListViewItem(new string[] { rom.Description, rom.Year, rom.Name, rom.Parent, rom.Direction, rom.Manufacturer, rom.Board }));
-                //sw1.WriteLine(rom.Name + "\t" + rom.Board + "\t" + rom.Parent + "\t" + rom.Direction + "\t" + rom.Description + "\t" + rom.Year + "\t" + rom.Manufacturer);
-            }
-            //sw1.Close();
-        }
-        private void loadRomList(IEnumerable<XElement> elements)
-        {
-            RomInfo.romList = new List<RomInfo>();
-            //StreamWriter sw1 = new StreamWriter("1.txt", false);
-            foreach (var ele in elements)
-            {
-                RomInfo rom = new RomInfo();
-                rom.Name = ele.Attribute("name").Value;
-                rom.Board = ele.Attribute("board").Value;
-                rom.Parent = ele.Element("parent").Value;
-                rom.Direction = ele.Element("direction").Value;
-                rom.Description = ele.Element("description").Value;
-                rom.Year = ele.Element("year").Value;
-                rom.Manufacturer = ele.Element("manufacturer").Value;
-                RomInfo.romList.Add(rom);
-                //loadform.listView1.Items.Add(new ListViewItem(new string[] { rom.Description, rom.Year, rom.Name, rom.Parent, rom.Direction, rom.Manufacturer, rom.Board }));
-                //sw1.WriteLine(rom.Name + "\t" + rom.Board + "\t" + rom.Parent + "\t" + rom.Direction + "\t" + rom.Description + "\t" + rom.Year + "\t" + rom.Manufacturer);
-            }
-            //sw1.Close();
-        }
 
-        private WaveFormat CreateWaveFormat()
-        {
-            WaveFormat format = new Microsoft.DirectX.DirectSound.WaveFormat();
-            format.AverageBytesPerSecond = 192000;
-            format.BitsPerSample = 16;
-            format.BlockAlign = 4;//(short)(format.Channels * (format.BitsPerSample / 8));
-            format.Channels = 2;
-            format.FormatTag = WaveFormatTag.Pcm;
-            format.SamplesPerSecond = 48000;
-            return format;
-        }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (Machine.bRom)
@@ -482,11 +176,7 @@ namespace ui
             Mame.exit_pending = true;
             Thread.Sleep(100);
             Generic.nvram_save();
-            /*if (Keyboard.dIDevice != null)
-            {
-                Keyboard.dIDevice.Dispose();
-                Keyboard.dIDevice = null;
-            }*/
+
             StreamWriter sw1 = new StreamWriter("mame.ini", false);
             sw1.WriteLine("[select]");
             sw1.WriteLine(sSelect);
@@ -504,22 +194,7 @@ namespace ui
                 //UI.cpurun();
                 Mame.mame_pause(true);
             }
-            //bRunning = false;
-            //pState = PlayState.PLAY_STOPPED;
-            //OnPlaying(pState);
-            //OnRunning(false);
-           /* foreach (ListViewItem lvi in loadform.listView1.Items)
-            {
-                if (sSelect == lvi.SubItems[2].Text)
-                {
-                    loadform.listView1.FocusedItem = lvi;
-                    lvi.Selected = true;
-                    loadform.listView1.TopItem = lvi;
-                    break;
-                }
-            }
-            //StopTimer();
-            loadform.ShowDialog();*/
+
         }
         private void resetToolStripMenuItem_Click(object sender, EventArgs e)
         {
